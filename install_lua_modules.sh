aa422af69b24f1e223767acc5f07554e4eeb112b
Clover_Midori <64039436+CloverMidori@users.noreply.github.com>
2021-02-04 07:43:28 +0900

    Update install_lua_modules.sh

--- install_lua_modules.sh
+++ install_lua_modules.sh
@@ -8,15 +8,15 @@ install_cudnn()
     rm -fr $CUDNN_WORK_DIR
     git clone https://github.com/soumith/cudnn.torch.git -b $CUDNN_BRANCH $CUDNN_WORK_DIR
     cd $CUDNN_WORK_DIR
-    luarocks make cudnn-scm-1.rockspec
+    /content/torch/install/bin/luarocks make cudnn-scm-1.rockspec
     cd ..
     rm -fr $CUDNN_WORK_DIR
 }
 
-luarocks install graphicsmagick
-luarocks install lua-csnappy
-luarocks install md5
-luarocks install uuid
-luarocks install csvigo
-install_cudnn
-PREFIX=$HOME/torch/install luarocks install turbo
+/content/torch/install/bin/luarocks install graphicsmagick
+/content/torch/install/bin/luarocks install lua-csnappy
+/content/torch/install/bin/luarocks install md5
+/content/torch/install/bin/luarocks install uuid
+/content/torch/install/bin/luarocks install csvigo
+/content/torch/install/bin/install_cudnn
+PREFIX=$HOME/torch/install /content/torch/install/bin/luarocks install turbo
